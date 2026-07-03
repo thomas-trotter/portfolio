b74d6e41822a201c912e45440847ea581d5b1bd4
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 15:15:56 +0100

    Add page.tsx

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "@/components/layout/page-layout";
+
+export default function BlogPage() {
+    return (
+        <PageLayout>
+            <div>
+                <h1>Blog</h1>
+            </div>
+        </PageLayout>
+    )
+}
