70b18cf38dddac4956538175a9d1004dc6e32c9b
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 14:43:34 +0100

    Add page.tsx

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "@/components/layout/page-layout"
+
+export default function ProjectsPage() {
+    return (
+        <PageLayout>
+            <p>Projects</p>
+        </PageLayout>
+    )
+}
