c96f3c02c27de56ac4495b94a421d0466da8cf23
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 15:00:23 +0100

    Add page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "@/components/layout/page-layout";
+
+export default function AboutPage() {
+    return (
+        <PageLayout>
+            <div>
+                <h1>About</h1>
+            </div>
+        </PageLayout>
+    )
+}
