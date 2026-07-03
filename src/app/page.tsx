ce5d30cf2fcb5cb95e7af971ebda8e778aee459f
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 08:52:16 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import TopBar from "@/components/ui/topbar";
+import PageLayout from "@/components/layout/page-layout";
 
 export default function Home() {
   return (
-    <div>
-      <TopBar />
-    </div>
+    <PageLayout>
+      <div>
+        <h2 className="text-display">Hi, I'm Thomas Trotter</h2>
+        <p className="text-body"> MSci AI Student - aspiring ML/software engineer building thoughtful, well-crafted systems</p>
+      </div>
+    </PageLayout>
   );
 }
