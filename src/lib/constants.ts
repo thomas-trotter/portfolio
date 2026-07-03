b4f57fab5105bed12d6269e9fc1d00014c57a3de
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 17:18:47 +0100

    Add constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const skills = [
+    "Communication",
+    "Teamwork",
+    "Python",
+    "Machine Learning",
+    "Software Development",
+] as const;
+
+export const profileLinks = [
+    { label: "GitHub", href: "https://github.com/thomas-trotter/thomas-trotter" },
+    { label: "LinkedIn", href: "https://www.linkedin.com/in/thomas-trotter/" },
+    { label: "Email", href: "mailto:thomas.trotter2006@gmail.com" },
+] as const;
