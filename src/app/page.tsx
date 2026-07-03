0b6ac324dd5afe8c35d36e109415fcece43d65ab
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 14:19:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import PageLayout from "@/components/layout/page-layout";
+import HeroSection from "@/components/sections/hero-section";
 
 export default function Home() {
   return (
     <PageLayout>
-      <div>
-        <h2 className="text-display">Hi, I'm Thomas Trotter</h2>
-        <p className="text-body"> MSci AI Student - aspiring ML/software engineer building thoughtful, well-crafted systems</p>
-      </div>
+      <HeroSection />
     </PageLayout>
   );
 }
