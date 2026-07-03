78f994add10bc1c43a72ab099bf53414581a3b32
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 14:33:35 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import PageLayout from "@/components/layout/page-layout";
+import FeaturedProjectsSection from "@/components/sections/featured-projects-section";
 import HeroSection from "@/components/sections/hero-section";
+import SkillsSection from "@/components/sections/skills-section";
 
 export default function Home() {
   return (
     <PageLayout>
       <HeroSection />
+      <FeaturedProjectsSection />
+      <SkillsSection />
     </PageLayout>
   );
 }
