404d678c4313a32a2d9563affe4d591b322798b2
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-03 15:35:16 +0100

    Update hero-section.tsx

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/ui/button";
 import Section from "@/components/ui/section";
-import { site } from "@/lib/site";
+import { site } from "@/lib/mocks/site";
 
 export default function HeroSection() {
   return (
