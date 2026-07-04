d17d17df9b9f5c1077a8dbffa105831561e6fac0
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-04 22:48:29 +0100

    Update apple-icon.tsx

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -1,32 +1,8 @@
-import { ImageResponse } from "next/og";
+import { createBrandIcon } from "@/lib/brand-icon";
 
-export const size = {
-  width: 180,
-  height: 180,
-};
-
+export const size = { width: 180, height: 180 };
 export const contentType = "image/png";
 
 export default function AppleIcon() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#4a5fc1",
-          color: "#fafaf9",
-          fontSize: 72,
-          fontWeight: 700,
-          borderRadius: "50%",
-        }}
-      >
-        TT
-      </div>
-    ),
-    size,
-  );
+  return createBrandIcon(size, 72);
 }
