aa5c231e66bf3ea11e937394787856998ce05c8f
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-04 23:50:17 +0100

    Update blog.tsx

--- src/components/mdx/blog.tsx
+++ src/components/mdx/blog.tsx
@@ -1,5 +1,11 @@
-import { mdxParagraphComponents } from "@/components/mdx/shared";
+import {
+  mdxHeadingComponents,
+  mdxLinkComponents,
+  mdxParagraphComponents,
+} from "@/components/mdx/shared";
 
 export const blogMdxComponents = {
+  ...mdxHeadingComponents,
+  ...mdxLinkComponents,
   p: mdxParagraphComponents.default,
 };
