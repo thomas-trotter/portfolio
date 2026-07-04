4f28047591b281c366a743a0fa50d38688a46c53
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-04 22:52:05 +0100

    Update content-image.tsx

--- src/components/ui/content-image.tsx
+++ src/components/ui/content-image.tsx
@@ -3,35 +3,32 @@ import classNames from "classnames";
 import Placeholder from "@/components/ui/placeholder";
 
 type ContentImageProps = {
-    src?: string;
-    alt: string;
-    className?: string;
-    fallbackLabel: string;
-}
+  src?: string;
+  alt: string;
+  className?: string;
+  fallbackLabel: string;
+};
 
 export default function ContentImage({
-    src,
-    alt,
-    className,
-    fallbackLabel
+  src,
+  alt,
+  className,
+  fallbackLabel,
 }: ContentImageProps) {
-    if (!src) {
-        return <Placeholder className={className}>{fallbackLabel}</Placeholder>
-    }
+  if (!src) {
+    return <Placeholder className={className}>{fallbackLabel}</Placeholder>;
+  }
 
-    return (
-        <div className={classNames(
-            "relative overflow-hidden",
-            className,
-        )}>
-            <Image
-                src={src}
-                alt={alt}
-                fill
-                loading="eager"
-                className="object-cover"
-                sizes="600px"
-            />
-        </div>
-    )
+  return (
+    <div className={classNames("relative overflow-hidden", className)}>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        loading="eager"
+        className="object-cover"
+        sizes="600px"
+      />
+    </div>
+  );
 }
