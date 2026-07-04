356667811fb028a8ce7f6a652bddaa76a5dcb4bd
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-04 22:29:48 +0100

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,9 +3,6 @@ import PageLayout from "@/components/layout/page-layout";
 import BlogPostDetailSection from "@/components/sections/blog-post-detail-section";
 import { getBlogPostBySlug } from "@/lib/content";
 
-export { getBlogPostMetadata as generateMetadata } from "@/lib/metadata";
-export { getBlogPostStaticParams as generateStaticParams } from "@/lib/static-params";
-
 type BlogPostDetailPageProps = {
   params: Promise<{
     slug: string
