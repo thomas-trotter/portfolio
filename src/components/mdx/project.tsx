64a1e0ea452f5a57a40deb10065315f16000d4e6
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-12 14:26:03 +0100

    Update project.tsx

--- src/components/mdx/project.tsx
+++ src/components/mdx/project.tsx
@@ -1,11 +1,11 @@
 import {
   mdxHeadingComponents,
-  mdxImageComponents,
+  mdxProjectImageComponents,
   mdxParagraphComponents,
 } from "@/components/mdx/shared";
 
 export const projectMdxComponents = {
   ...mdxHeadingComponents,
-  ...mdxImageComponents,
+  ...mdxProjectImageComponents,
   p: mdxParagraphComponents.plain,
 };
